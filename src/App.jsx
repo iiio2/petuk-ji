795358b5f982a8a29136959f0ab968c6d078b3c3
mr.robot <gomexxxx71@gmail.com>
2022-10-07 21:59:47 +0600

    bootstrap & font added.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AboutUs from "./components/about";
+
+const App = () => {
+  return (
+    <>
+      <h3>hello world</h3>
+      <AboutUs />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/about.jsx
@@ -0,0 +1,9 @@
+const AboutUs = () => {
+  return (
+    <>
+      <h3>About Us</h3>
+    </>
+  );
+};
+
+export default AboutUs;
