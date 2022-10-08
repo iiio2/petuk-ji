ac7f45aba7c7eb95a90dd734f52ad77f8531dac4
mr.robot <gomexxxx71@gmail.com>
2022-10-08 19:06:14 +0600

    environment variables added.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjfHGht5-_YypNHgNtPg5Kn4tRXXBPE58",
-  authDomain: "petuk-ji-47765.firebaseapp.com",
-  projectId: "petuk-ji-47765",
-  storageBucket: "petuk-ji-47765.appspot.com",
-  messagingSenderId: "409467953606",
-  appId: "1:409467953606:web:1a2973d27e4a01c60212c7",
+  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+  authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${import.meta.env.VITE_FIREBASE_DATABASE_URL}`,
+  projectId: `${import.meta.env.VITE_PROJECT_ID}`,
+  storageBucket: `${import.meta.env.VITE_SOTRAGE_BUCKET}`,
+  messagingSenderId: `${import.meta.env.VITE_MESSAGING_SENDER_ID}`,
+  appId: `${import.meta.env.VITE_APP_ID}`,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
